e431347eeed32f1848dfff8708a7b8d46beeac78
KPAMsar <kpamsarshija@gmail.com>
2024-01-20 22:17:58 +0100

    fix:task table fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -77,8 +77,8 @@ const Login = () => {
   const [alltask, setAllTask] = useState(true);
   return (
     <>
-      <div className=" lg:mb-[6rem] w-[100dvw] lg:w-[40dvw] overflow-hidden md:pl-[30px] pr-[10px] pl-[10px] md:pr-[30px] lg:pl-[48px]   lg:pr-[40px] fixed ">
-        <div class="bg-slate-400 flex flex-row justify-between  items-center   h-[60px] mt-4   ">
+      <div className=" lg:mb-[6rem] w-[100dvw]  overflow-hidden md:pl-[30px] pr-[10px] pl-[10px] md:pr-[30px] lg:pl-[48px]   lg:pr-[40px] fixed ">
+        <div class="bg-slate-300 flex flex-row  lg:mx-auto lg:w-[40dvw] justify-between  items-center   h-[60px] mt-4   ">
           <div
             className={`${
               alltask
@@ -89,27 +89,32 @@ const Login = () => {
               setAllTask(true);
             }}
           >
-            <p className="text-[600]  items-center justify-center flex ">
+            <p className="text-[16px] font-[700]  items-center justify-center flex ">
               All Tasks
             </p>
           </div>
-          <p
+          <div
             onClick={() => {
               setAllTask(false);
             }}
-            className={`${
+            className={` ${
               !alltask
-                ? "bg-slate-100 w-[50%] p-5 mr-1 items-center justify-center flex h-[80%] rounded-sm text-[#00356B]"
+                ? "bg-slate-100 w-[50%] p-5 mr-1 items-center justify-center flex h-[80%] rounded-sm  text-[#00356B]"
                 : "w-[50%] p-5 ml-1 items-center justify-center flex h-[80%] text-[#00356B]"
             }`}
           >
-            Completed Tasks
-          </p>
+            <p className="text-[16px] font-[700]  items-center justify-center flex ">
+              Completed Tasks
+            </p>
+          </div>
         </div>
 
-        <div>
+        <div className="mt-4 lg:w-[70dvw] lg:mx-auto">
+          <p className="text-[#00356B] text-[20px]   font-[700] lg:text-[40px]  ">
+            {alltask ? "All Tasks" : " Completed Tasks"}
+          </p>
           <table className="table w-[100dvw] overflow-hidden lg:w-full lg:pb-[60px]">
-            <thead className="text-white">
+            <thead className="text-[#00356B]">
               <tr>
                 <th className="min-w-[10px]">s/n</th>
                 <th className="min-w-[120px]">Task</th>
@@ -117,7 +122,7 @@ const Login = () => {
                 <th className="min-w-[50px]">Actions</th>
               </tr>
             </thead>
-            <tbody className=" ">
+            <tbody className=" text-[#00356B]">
               {data.map((item, index) => (
                 <tr
                   key={index}
